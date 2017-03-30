858b34059adf0d4058b4ea5a25f0734219ccdd80
Katie Dahlin <pastry.katie@gmail.com>
2017-03-29 21:36:25 -0600

    created out post and findById methods

--- server.js
+++ server.js
@@ -1,27 +1,61 @@
 var express = require('express');
 var Superhero = require('./models/superhero');
 var app = express();
+var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 //required to connect to our local database.
 //uit will look for/create a database called superheroes
 //make sure you are running your nodemon and mongod servers!
 mongoose.connect("mongodb://localhost/superheroes");
-app.get('/test', function(req, res){
-  res.send("You found the test route");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+
+//return all superheroes
+app.get('/superheroes', function(req, res){
+  Superhero.find(function(err, data){
+    if(err){
+      console.log('You have an error!');
+    }else{
+      res.json(data);
+    }
+
+
+  });
+
+});
+
+app.post('/superheroes', function(req, res){
+  var newSuper = new Superhero({
+      name:       req.body.name,
+      superPower: req.body.superPower,
+      universe:   req.body.universe,
+      evil:       req.body.evil,
+      rank:       req.body.rank,
+  });
+  newSuper.save(function(err, sh){
+    if(err){
+      console.log(err)
+    }else{
+      res.json(sh)
+    }
+  });
 })
 
-var superHero1 = {
-  name: "Superman",
-  superPower: 'I can fly!',
-  universe: 'DC',
-  evil: false,
-  rank:10
-};
-
-app.get('/superhero', function(req, res){
-  res.json(superHero1);
+app.get('/superheroes/:superhero_id', function(req, res){
+  Superhero.findById(req.params.superhero_id, function(err, data){
+    if(err){
+      console.log(err);
+    }else{
+      res.json(data);
+    }
+  })
 });
+//app.delete
+//app.put
+
 var server = app.listen(3000, function(){
   console.log('Starting server on port 3000⚡️')
 });
